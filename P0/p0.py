0f269ae44a4cac59a3f39b8b97cff01bb187ef34
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-19 14:11:26 -0600

    Update p0.py

--- P0/p0.py
+++ P0/p0.py
@@ -1,1 +1,20 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+x=10
+R=12
+x+R
+22
+raul=14
+raul**3
+2744
+lista=[2,9,12,45]
+lista
+[2, 9, 12, 45]
+min(lista)
+2
+
+
+
+================== RESTART: /Users/raullagunes/Documents/P0.py =================
+2
 
