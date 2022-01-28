1b2235ed1318926c0c0f23b8e4f644f38199ed8b
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-28 11:52:12 -0600

    Add files via upload

---
+++ P0/E6.R
@@ -0,0 +1,44 @@
+a<-3
+b<-4
+c<-c(5,6,7)
+d <- c(8, 10, 12)
+print(a + b)
+
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
+
+print(a^b)
+
+
+
+print(b^a)
+
+print(a**b)
+print(a+c)
+
+print(c+b)
+
+print(c+d)
+
+print(c * d)
+
+print(c %*% d)
+
+e <- matrix(c(1, 2, 3, 4), nrow=2)
+print(e)
+
+f <- matrix(c(2, 4, 6, 8), nrow=2)
+print(f)
+
+print(e + f)
+     
+print(e - f)
+
+print(e * f)
+     
+print(e %*% f)
+     
