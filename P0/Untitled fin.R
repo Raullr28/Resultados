f13ca09ac46c384c2959df4ef88a58059618ef52
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-28 17:15:38 -0600

    Add files via upload

---
+++ P0/Untitled fin.R
@@ -0,0 +1,64 @@
+####################
+#                  #
+# Copia todo esto! #
+#                  #
+####################
+# Hecho con gusto por Rafa @GonzalezGouveia
+ 
+# T2V7 - importar datos csv a R
+ 
+# Objetivo: aprender a importar archivos csv a R
+# ------------------------------------------------
+# En este ejercicio vamos a aprender:
+# 1. Que necesitas antes de empezar
+# 2. Cómo importar datos utilizando código de R
+# 3. Cómo importar datos con la interfaz de RStudio
+ 
+ 
+#####################################
+# 1. Que necesitas antes de empezar #
+#####################################
+ 
+# instalar paquete readr
+# install.packages("readr")
+ 
+# cargar paquete readr
+library(readr)
+ 
+# buscar la ruta del archivo de csv
+file.choose()
+ 
+# Copiar ruta de la consola y guardar en variable
+ 
+ 
+ 
+#####################################
+# 2. importar csv con código de R #
+#####################################
+ 
+# importar datos gapminder
+ 
+ 
+# mirar datos
+ 
+ 
+# caso no titulo
+ 
+ 
+# para agregar nombre
+ 
+ 
+# caso punto y coma
+ 
+ 
+########################################
+# 3. importar csv con interfaz RStudio #
+########################################
+ 
+# ir a File > Import Dataset > From Text (readr)
+ 
+# código que genera y ejecuta R automáticamente
+ 
+ 
+ 
+# fin
