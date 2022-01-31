5ad282d99bf1f5a7144e2064b0844534e9a3e688
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-30 22:10:10 -0600

    Update E14.R

--- P0/E14.R
+++ P0/E14.R
@@ -1,1 +1,9 @@
-x <- 1:20 y <- sin(x) print(plot(x, y)) prit(plot(x, y, type="l")) print(lines(x, cos(x), col="red")) print(abline(h=0, lwd=5, col="green")) print(plot(x, y)) print(points(x + 1, cos(x + 1), pch=15, col="green")) print(plot(x, y, main="Texto arriba", xlab="Etiqueta", ylab="Otra etiqueta"))
+x <- 1:20
+y <- sin(x)
+print(plot(x, y))
+plot(x, y, type="l")
+print(lines(x, cos(x), col="red"))
+print(abline(h=0, lwd=5, col="green"))
+print(plot(x, y))
+print(points(x + 1, cos(x + 1), pch=15, col="green"))
+print(plot(x, y, main="Texto arriba", xlab="Etiqueta", ylab="Otra etiqueta"))
