8f252303ced9f58380b79a93a0827c1a9815812d
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-31 13:20:10 -0600

    Add files via upload

---
+++ P0/creaciòn de subrutinas.py
@@ -0,0 +1,13 @@
+def potdos(x):
+    return 2**x
+
+print(potdos(3))
+
+from math import sqrt
+def eucl(x1, y1, x2, y2):
+    dx = x1 - x2
+    dy = y1 - y2
+    return sqrt(dx**2 + dy**2)
+
+print(eucl(1, 2, 3, 4))
+
