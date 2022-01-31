7c318e6cc9068ec5fe1ac8fa5e82c10f4b4a506a
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-01-31 10:10:36 -0600

    Create resultado. medir.R

---
+++ P0/RP0/resultado. medir.R
@@ -0,0 +1,10 @@
+source("/Users/raullagunes/Desktop/medir.R")
+512 0.019 
+tiempo 512 0.01 
+memoria 512 2097368 
+1024 0.05 
+tiempo 1024 0.041 
+memoria 1024 8388824 
+2048 0.189 
+tiempo 2048 0.156 
+memoria 2048 33554648 
