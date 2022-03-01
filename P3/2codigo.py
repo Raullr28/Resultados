c7adbbbd80db109f5f5d36d24cf4471e46fae3e5
Raullr28 <97633720+Raullr28@users.noreply.github.com>
2022-03-01 17:02:03 -0600

    Add files via upload

---
+++ P3/2codigo.py
@@ -0,0 +1,108 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 2, in <module>
+    import seaborn as sns
+ModuleNotFoundError: No module named 'seaborn'
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 4, in <module>
+    import cv2
+ModuleNotFoundError: No module named 'cv2'
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 56, in <module>
+    bp1=[org[0],org[1],org[2]]
+IndexError: list index out of range
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 60, in <module>
+    pb=plt.boxplot(bp1,bp2,bp3)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/pyplot.py", line 2428, in boxplot
+    return gca().boxplot(
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/__init__.py", line 1412, in inner
+    return func(ax, *map(sanitize_sequence, args), **kwargs)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/axes/_axes.py", line 3766, in boxplot
+    _, marker, color = _process_plot_format(sym)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/axes/_base.py", line 170, in _process_plot_format
+    if fmt[i:i+2] in mlines.lineStyles:  # First, the two-char styles.
+TypeError: unhashable type: 'list'
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 60, in <module>
+    pb=plt.boxplot([bp1,bp2,bp3])
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/pyplot.py", line 2428, in boxplot
+    return gca().boxplot(
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/__init__.py", line 1412, in inner
+    return func(ax, *map(sanitize_sequence, args), **kwargs)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/axes/_axes.py", line 3711, in boxplot
+    bxpstats = cbook.boxplot_stats(x, whis=whis, bootstrap=bootstrap,
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/cbook/__init__.py", line 1155, in boxplot_stats
+    X = _reshape_2D(X, "X")
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/matplotlib/cbook/__init__.py", line 1398, in _reshape_2D
+    raise ValueError(f'{name} must have 2 or fewer dimensions')
+ValueError: X must have 2 or fewer dimensions
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+Traceback (most recent call last):
+  File "/Users/raullagunes/Downloads/pract3_codigo.py", line 60, in <module>
+    pb=plt.boxplot([org[0],org[1],inv[0],inv[1],ale[0],ale[1]])
+IndexError: list index out of range
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
+
+============ RESTART: /Users/raullagunes/Downloads/pract3_codigo.py ============
+############ CORE: 3 ##################
+############ CORE: 2 ##################
